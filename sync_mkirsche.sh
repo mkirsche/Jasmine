1819d72e8ee71736285722aaf8d7dd9849833485
mkirsche <jnredotter@gmail.com>
2019-11-25 13:36:02 -0500

    Removed unneeded bash script

--- sync_mkirsche.sh
+++
@@ -1,7 +0,0 @@
-# Used to sync pre- and post-processing scripts from other projects in Melanie's workspace
-cp ../SVPrePostProcessing/src/DuplicationsToInsertions.java  preprocessing
-cp ../SVPrePostProcessing/src/GenomeQuery.java  preprocessing
-cp ../SVPrePostProcessing/src/VcfEntry.java  preprocessing
-cp ../SVPrePostProcessing/src/MarkSpecificCalls.java  preprocessing
-cp ../SVPrePostProcessing/src/InsertionsToDuplications.java  postprocessing
-cp ../SVPrePostProcessing/src/VcfEntry.java  postprocessing
