f9dff2a081d6690948c0d1c303d9af9633f5b1fb
mkirsche <jnredotter@gmail.com>
2019-11-30 22:39:58 -0500

    Adding running script

---
+++ run.sh
@@ -0,0 +1,8 @@
+# Script for running Jasmine
+if [ "$(uname -s)" = 'Linux' ]; then
+    BINDIR=$(dirname "$(readlink -f "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
+else
+    BINDIR=$(dirname "$(readlink "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
+fi
+
+java -cp $BINDIR/src:$BINDIR/Iris/src Main "${@:1}"
